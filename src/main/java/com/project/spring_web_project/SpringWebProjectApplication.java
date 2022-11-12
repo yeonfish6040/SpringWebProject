b36835694af3702e76fac38f5a2ce586d6e112da
yeonfish6040 <72007187+yeonfish6040@users.noreply.github.com>
2022-11-12 12:36:12 +0900

    upload

---
+++ src/main/java/com/project/spring_web_project/SpringWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.spring_web_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebProjectApplication.class, args);
+    }
+
+}
